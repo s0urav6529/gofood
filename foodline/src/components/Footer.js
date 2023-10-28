a5ddbb03fc5fcb57baa0242df3972bf1f9497565
souravmajumder <sourav.cse18.duet@gmail.com>
2023-10-29 01:19:19 +0600

    add some components

--- foodline/src/components/Footer.js
+++ foodline/src/components/Footer.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          <NavLink
+            href="/"
+            className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+          >
+          </NavLink>
+          <span className="mb-3 mb-md-0 text-muted">© 2023 Foodline, Inc</span>
+        </div>
+
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+        </ul>
+      </footer>
+    </div>
+  );
 }
--- foodline/src/components/Navbar.js
+++ foodline/src/components/Navbar.js
@@ -1,20 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Navbar</a>
+                <NavLink className="navbar-brand fs-1 fst-italic" to="/">Foodline</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                    <a className="nav-link" href="/">Features</a>
-                    <a className="nav-link" href="/">Pricing</a>
-                    <a className="nav-link disabled">Disabled</a>
+                    <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                    <NavLink className="nav-link" to="/login">Login</NavLink>
                 </div>
                 </div>
             </div>
